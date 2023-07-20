b389f4a68e0318e0211731a36da1671f8a8e040e
Ebuka Peter <peterdbrainy5@gmail.com>
2023-07-20 16:23:46 +0100

    implement reactive form control and validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-forms';
+  form: FormGroup = this.fb.group([]);
+  forbiddenProjectName = ['Test'];
+  projectStatus: Array<string> = [
+    "Stable",
+    "Critical",
+    "Finished"
+  ]
+
+  constructor(
+    private fb: FormBuilder
+  ){
+  }
+
+  ngOnInit() {
+   this.form = new FormGroup({
+    project_name: new FormControl(null, [Validators.required,
+      this.forbiddenNames.bind(this)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    project_status: new FormControl(null, Validators.required)
+   }) 
+   console.log('show me form', this.form);
+   console.log('show me form controls',  this.form.get('project_name')?.hasError('required'));
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } | null {
+    if (this.forbiddenProjectName.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
+  }
+
+  onSubmit():void {
+    console.log(this.form);
+  }
 }
